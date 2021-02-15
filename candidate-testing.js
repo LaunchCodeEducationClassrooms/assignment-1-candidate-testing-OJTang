6aee55d6e77b80425628586ee036b9b19e1f9fdf
OJTang <oliverwunderlich13@gmail.com>
2021-02-15 18:35:38 +0000

    Made more spacing changes to fit with tests

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ let candidateName = '';
 let question = 'Who was the first American woman in space? ';
 let correctAnswer = 'Sally Ride';
 let candidateAnswer = '';
-let questions = ['Who was the first American woman in space? ', 'True or false: 5000 meters = 5 kilometers. ', '(5+3)/2*10 = ? ', 'Given the array [8, \'Orbit\', \'Trajectory\', 45], what entry is at index 2? ', 'What is the minimum crew size for the ISS? '];
+let questions = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters. ', '(5 + 3)/2 * 10 = ? ', 'Given the array [8, \'Orbit\', \'Trajectory\', 45], what entry is at index 2? ', 'What is the minimum crew size for the ISS? '];
 let correctAnswers = ['Sally Ride', 'True', '40', 'Trajectory', '3'];
 let candidateAnswers = [];
 
@@ -42,7 +42,7 @@ for (j = 0; j < correctAnswers.length; j++) {
   let grade = total / questions.length *100;
   
   return grade;
-  
+
   console.log(`*** Overall grade: ${grade}% \(${total} out of ${questions.length} correct\) ***`);
   if (grade >= 80) {
     console.log(`*** Status: PASSED  ***`);
